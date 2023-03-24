f45e2ec6cf95d17d5585ed8d6f91818fcff6b779
Ankita Ranjan <ankita@fynarfin.io>
2023-03-24 16:48:48 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:13
+EXPOSE 8080
+
+COPY build/libs/*.jar .
+CMD java -jar *.jar
---
+++ src/main/java/org/mifos/connector/GsmaChannelSimulatorApplication.java
@@ -0,0 +1,66 @@
+package org.mifos.connector;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.threeten.bp.Instant;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.mifos.connector.gsmaChannelSimulator.configuration.CustomInstantDeserializer;
+import org.mifos.connector.gsmaChannelSimulator.configuration.LocalDateConverter;
+import org.mifos.connector.gsmaChannelSimulator.configuration.LocalDateTimeConverter;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.OffsetDateTime;
+import org.threeten.bp.ZonedDateTime;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "GSMA Channel Simulator connector APIs"))
+public class GsmaChannelSimulatorApplication {
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		JavaTimeModule javaTimeModule = new JavaTimeModule();
+		javaTimeModule.addDeserializer(Instant.class, CustomInstantDeserializer.INSTANT);
+		javaTimeModule.addDeserializer(OffsetDateTime.class, CustomInstantDeserializer.OFFSET_DATE_TIME);
+		javaTimeModule.addDeserializer(ZonedDateTime.class, CustomInstantDeserializer.ZONED_DATE_TIME);
+		/*javaTimeModule.addSerializer(LocalDate.class, new CustomInstantDeserializer.LocalDateSerializer());*/
+		javaTimeModule.addDeserializer(LocalDate.class, new CustomInstantDeserializer.LocalDateDeserializer());
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(javaTimeModule);
+		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		return objectMapper
+				.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+				.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+				.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Configuration
+	static class CustomDateConfig implements WebMvcConfigurer {
+		@Override
+		public void addFormatters(FormatterRegistry registry) {
+			registry.addConverter(new LocalDateConverter("yyyy-MM-dd"));
+			registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+			//registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+		}
+	}
+	public static void main(String[] args) {
+
+		SpringApplication.run(GsmaChannelSimulatorApplication.class, args);
+	}
+
+}
